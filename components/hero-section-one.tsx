2983cbd986a223664d0a8209cb85eb5a6d2efbcd
robin broos <robin1broos@gmail.com>
2025-12-08 11:57:31 +0100

    nieuwe changes

--- components/hero-section-one.tsx
+++ components/hero-section-one.tsx
@@ -33,9 +33,6 @@ export default function HeroSection() {
                         <CirclePlay className="fill-primary/25 stroke-primary" />
                         <span className="text-nowrap">Bekijk onze blogs</span>
                       </Link>{" "}
-                      <Link href="https://powderblue-turtle-512586.hostingersite.com/docs/api#/">
-                        <span className="text-nowrap">api docs</span>
-                      </Link>
                     </Button>
                   </div>
                 </div>
